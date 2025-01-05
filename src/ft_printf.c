2448885a11ba624bdb4d08b28a8b7096377cccf0
sasha (42) <facosta@student.42madrid.com>
2025-01-05 12:43:45 +0100

    🔥 rm main.c script

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 22:37:42 by facosta           #+#    #+#             */
-/*   Updated: 2025/01/02 00:04:31 by facosta          ###   ########.fr       */
+/*   Updated: 2025/01/05 12:42:18 by facosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,3 +66,102 @@ int	ft_printf(char const *ct_f_str, ...)
 	va_end(args);
 	return (print_length);
 }
+
+
+// #include <stdio.h>
+// #include "../include/ft_printf.h"
+
+// int	main(void)
+// {
+// 	{
+// 		// TEST empty string
+// 		int yo = ft_printf("");
+// 		printf("\n");
+// 		int og = printf("");
+// 	}
+
+// 	{
+// 		// TEST NULL PTR as format string
+// 		int yo = ft_printf(NULL);
+// 		printf("\n");
+// 		int og = printf(NULL);
+// 	}
+
+// 	{
+// 		// TEST char(s)
+// 		int yo = ft_printf("hola mund%c!%c\n", 'u', 'n', 'o');
+// 		printf("\n");
+// 		int og = printf("hola mund%c!%c\n", 'u', 'n', 'o');
+// 		// printf("yo: %d // og: %d", yo, og);
+// 	}
+
+// 	{
+// 		// TEST string
+// 		int yo = ft_printf("hola mu%s4", "nd0\n");
+// 		printf("\n");
+// 		int og = printf("hola mu%s5", "nd0\n");
+// 		// printf("yo: %d // og: %d", yo, og);
+// 	}
+
+// 	{
+// 		// TEST null string
+// 		char *patata = NULL;
+// 		char valid[11] = "hola mu%s!";
+// 		int yo = ft_printf(valid, patata);
+// 		printf("\n");
+// 		int og = printf(valid, patata);
+// 		printf("yo: %d // og: %d", 0, og);
+// 	}
+
+// 	{
+// 		// TEST decimal numbers w/ d
+// 		int yo = ft_printf("come tarta: 3.%d", 1415926);
+// 		printf("\n");
+// 		int og = printf("come tarta: 3.%d", 1415926);
+// 		printf("yo: %d // og: %d", yo, og);
+// 	}
+
+// 	{
+// 		// TEST decimal numbers w/ i
+// 		int yo = ft_printf("come tarta: 3.%i", -1415926);
+// 		printf("\n");
+// 		int og = printf("come tarta: 3.%i", -1415926);
+// 		printf("yo: %d // og: %d", yo, og);
+// 	}
+
+// 	{
+// 		// TEST unsigned number
+// 		unsigned int unum = 12303;
+// 		int yo = ft_printf("sin signo!: %u", unum);
+// 		printf("\n");
+// 		int og = printf("sin signo!: %u", unum);
+// 		printf("yo: %d // og: %d", yo, og);
+// 	}
+
+// 	{
+// 		// TEST unsigned number in hexdec
+// 		unsigned int unum = 12303;
+// 		int yo = ft_printf("sin signo!: %X", unum);
+// 		printf("\n");
+// 		int og = printf("sin signo!: %X", unum);
+// 		printf("yo: %d // og: %d", yo, og);
+// 	}
+
+// 	{
+// 		// TEST unsigned number in hexdec
+// 		unsigned long long unum = (unsigned long long) 18446744073709551620;
+// 		int yo = ft_printf("sin signo!: %u", unum);
+// 		printf("\n");
+// 		int og = printf("sin signo!: %u", unum);
+// 		printf("yo: %d // og: %d", yo, og);
+// 	}
+
+// 	{
+// 		// TEST unsigned number in hexdec
+// 		int yo = ft_printf("sin signo!: %p", 0);
+// 		printf("\n");
+// 		int og = ft_printf("sin signo!: %p", 0);
+// 		printf("yo: %d // og: %d", yo, og);
+// 	}
+// 	return (0);
+// }
